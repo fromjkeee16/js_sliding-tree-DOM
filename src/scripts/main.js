2d45bbf46276177b961a742eeb9ca877769a0629
fromjkeee16 <fromjkeee16@gmail.com>
2025-03-31 15:19:49 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const root = document.querySelector('.tree');
+
+createExpandableTree(root);
+
+function createExpandableTree(element) {
+  for (const child of element.children) {
+    const innerUl = child.querySelector('ul');
+
+    if (!innerUl) {
+      return;
+    }
+
+    const textNode = findTextNode(child);
+    let title;
+
+    if (textNode) {
+      title = document.createElement('span');
+      title.textContent = textNode.textContent.trim();
+      textNode.replaceWith(title);
+    }
+
+    title.addEventListener('click', () => {
+      innerUl.style.display = innerUl.style.display === 'none' ? '' : 'none';
+    });
+    createExpandableTree(innerUl);
+  }
+}
+
+function findTextNode(element) {
+  for (const child of element.childNodes) {
+    if (child.nodeType === Node.TEXT_NODE && child.textContent.trim()) {
+      return child;
+    }
+  }
+
+  return null;
+}
